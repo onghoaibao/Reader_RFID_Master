a7dd230b30eee5fe4484e76183595c9ced746720
onghoaibao <bao1305946@gmail.com>
2020-04-22 15:06:27 +0700

    Update UI Java

--- Reader_RFID/src/reader_rfid/MainJFrame.java
+++ Reader_RFID/src/reader_rfid/MainJFrame.java
@@ -5,6 +5,29 @@
  */
 package reader_rfid;
 
+import static com.sun.xml.internal.fastinfoset.alphabet.BuiltInRestrictedAlphabets.table;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.BorderFactory;
+import javax.swing.JComponent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.border.Border;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.JTableHeader;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
+
 /**
  *
  * @author bao13
@@ -14,8 +37,41 @@ public class MainJFrame extends javax.swing.JFrame {
     /**
      * Creates new form MainJFrame
      */
+    private JTable jTableShowDataBase;
+    private static int ii = 1;
+    private JScrollPane scrollpane;
+    private DefaultTableModel dm;
+        
     public MainJFrame() {
+        getContentPane().setBackground(Color.WHITE);
+        scrollpane = new JScrollPane(jTableData);
         initComponents();
+        setTitle("Giam Sat Thiet Bi Y Te");
+        configTable();
+        //ableData.
+        for(int y=0; y<10; y++){
+            setRowDataToTable(y, String.valueOf(y+1), "Máy siêu âm màu (01 máy)\n112233/logicE\nGE/Mỹ",
+                           "15/06/2000;\n999.999.000;\nMediruop",
+                           "Tot",
+                           "Nội tổng quát\n01/011/2020",
+                           "01/02/2020",
+                           "01/01/2020: Nội Tổng quát\n01/02/2020 trả\n15/02/2020:\nNội thần kinh mượn",
+                           "Nội thần kinh");
+            ii = y;
+        }
+        
+        System.out.println();
+        jTableData.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+        public void valueChanged(ListSelectionEvent event) {
+            // do some actions here, for example
+            // print first column value from selected row
+            if(!event.getValueIsAdjusting()){
+                for(int i=0; i<8;i++)
+                System.out.println(jTableData.getValueAt(jTableData.getSelectedRow(), i).toString());
+                
+            }
+        }
+    });
     }
 
     /**
@@ -27,43 +83,103 @@ public class MainJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanelTable = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        jTableData = new javax.swing.JTable();
+        jPanel1 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 255, 255));
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        jPanelTable.setBackground(new java.awt.Color(255, 255, 255));
+        jPanelTable.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jTableData.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jTableData.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Title 1", "Title 2", "Title 3", "Title 4", "Title 5", "Title 6", "Title 7", "Title 8"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jTableData.setGridColor(new java.awt.Color(255, 255, 255));
+        jScrollPane1.setViewportView(jTableData);
+
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+
+        jButton1.setText("Them moi");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(78, 78, 78)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        javax.swing.GroupLayout jPanelTableLayout = new javax.swing.GroupLayout(jPanelTable);
+        jPanelTable.setLayout(jPanelTableLayout);
+        jPanelTableLayout.setHorizontalGroup(
+            jPanelTableLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelTableLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 910, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanelTableLayout.setVerticalGroup(
+            jPanelTableLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanelTableLayout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanelTableLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 554, Short.MAX_VALUE)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(22, 22, 22)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 559, Short.MAX_VALUE)
-                .addGap(617, 617, 617))
+                .addContainerGap()
+                .addComponent(jPanelTable, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)
-                .addGap(24, 24, 24))
+                .addComponent(jPanelTable, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        ii++;
+        jTableData.setValueAt(String.valueOf(ii), 0, 0);
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -101,7 +217,80 @@ public class MainJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanelTable;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTableData;
     // End of variables declaration//GEN-END:variables
+
+    private void configTable() {
+        //configTable();
+        jTableData.setBackground(Color.WHITE);
+        jTableData.getTableHeader().setPreferredSize(new Dimension(50, 70));
+        jTableData.setShowGrid(true);
+        jTableData.setRowHeight(30);
+        jTableData.setShowHorizontalLines(true); // only HorizontalLines
+        jTableData.setShowVerticalLines(true); //  only VerticalLines
+        jTableData.setShowGrid(true);          // show Horizontal and Vertical
+        
+        dm = new DefaultTableModel() {
+            public Class<String> getColumnClass(int columnIndex) {
+                return String.class;
+            }
+
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+
+        dm.setDataVector(
+                new Object[][]{
+                    {}, {}, {}, {}
+                },
+                new Object[]{"STT",
+                             "Tên thiết bị, số lượng\nSố seri/model\nHãng/nước sx",
+                             "Ngày sx;\nNguyên giá;\nCty nhập",
+                             "Tình trạng hoạt động",
+                             "Đơn vị  mượn\nNgày mượn",
+                             "Ngày trả",
+                             "Lịch sử di chuyển", "Hiện tại"});
+       
+        jTableData.setModel(dm);
+
+        MultiLineHeaderRenderer renderer = new MultiLineHeaderRenderer();
+        Enumeration e = jTableData.getColumnModel().getColumns();
+        while (e.hasMoreElements()) {
+            ((TableColumn) e.nextElement()).setHeaderRenderer(renderer);
+        }
+        jTableData.getColumnModel().getColumn(0).setPreferredWidth(27);
+        jTableData.getColumnModel().getColumn(1).setPreferredWidth(120);
+        jTableData.getColumnModel().getColumn(2).setPreferredWidth(100);
+        jTableData.getColumnModel().getColumn(3).setPreferredWidth(90);
+        jTableData.getColumnModel().getColumn(4).setPreferredWidth(90);
+        jTableData.getColumnModel().getColumn(5).setPreferredWidth(120);
+        jTableData.getColumnModel().getColumn(6).setPreferredWidth(100);
+        jTableData.getColumnModel().getColumn(7).setPreferredWidth(100);
+        jTableData.setDefaultRenderer(String.class, new MultiLineTableCellRenderer());
+        TableRowSorter<? extends TableModel> sort = new TableRowSorter<DefaultTableModel>(dm);
+        jTableData.setRowSorter(sort);
+        getContentPane().add(scrollpane);
+    }
+    
+    private void  setRowDataToTable(int row, 
+                                    String stt, String dv, String sx,
+                                    String tthd, String dvm_nm, String nt,
+                                    String lsdc, String ht){
+        dm.addRow(new Object[]{});
+        jTableData.setModel(dm);
+        jTableData.setValueAt(stt, row, 0);
+        jTableData.setValueAt(dv, row, 1);
+        jTableData.setValueAt(sx, row, 2);
+        jTableData.setValueAt(tthd, row, 3);
+        jTableData.setValueAt(dvm_nm, row, 4);
+        jTableData.setValueAt(nt, row, 5);
+        jTableData.setValueAt(lsdc, row, 6);
+        jTableData.setValueAt(ht, row, 7);
+    }
+    
 }
---
+++ Reader_RFID/src/reader_rfid/test1.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reader_rfid;
+
+/**
+ *
+ * @author bao13
+ */
+public class test1 {
+    
+}
